be96dfcac5ff96cbe7957c360de354d25fbcd683
Fabian Vilers <fabian.vilers@dev-one.com>
2021-01-05 11:32:39 +0100

    Add the HomePage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from "react";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return (
-    <div>
-      <h1>Twitter clone</h1>
-    </div>
-  );
+  return <HomePage />;
 }
 
 export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Twitter clone</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus cum
+        quasi voluptatum similique magni eveniet, quibusdam facilis minus
+        tenetur perspiciatis impedit repudiandae molestias amet vero
+        voluptatibus officia quis architecto! Odio!
+      </p>
+    </div>
+  );
+}
+
+export default HomePage;
