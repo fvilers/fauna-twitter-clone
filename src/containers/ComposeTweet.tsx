4c3d3899cf628865f3791256b417387baa77f371
Fabian Vilers <fabian.vilers@dev-one.com>
2021-01-05 15:23:30 +0100

    Add the ComposeTweet component

---
+++ src/containers/ComposeTweet.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ComposeTweet() {
+  // TODO: handle send tweet
+  return <div></div>;
+}
+
+export default ComposeTweet;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import Nav from "../components/Nav";
+import ComposeTweet from "../containers/ComposeTweet";
+import AuthContext from "../contexts/AuthContext";
 
 function HomePage() {
+  const { hasSecret } = useContext(AuthContext);
   return (
     <div>
       <h1>Twitter clone</h1>
       <Nav />
+      {hasSecret() && <ComposeTweet />}
     </div>
   );
 }
